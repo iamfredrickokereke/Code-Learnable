05c6bef3cef475696e55dc8de3bab9d00d662cc4
Kelly Online <Kellyonline2016@gmail.com>
2019-04-23 23:34:13 +0100

    App source code file added

---
+++ PreLearnable/JS-Apps/MatLib-Pi-Fib-Generator/code.js
@@ -0,0 +1,14 @@
+function getPI(iterations){
+    let pi = 0; divisor = 1;
+
+    for(i = 0; i <= iterations; i++){
+
+       pi= pi + (4/divisor) - (4/(divisor + 2))
+    } 
+
+    divisor = divisor + 4;
+}
+
+
+
+pi = 4/1 - 4/3 + 4/5 - 4/7 .toExponential.apply..
