14e9647254dcbbf38435f4c48a8aba4ab10f7728
Kelly Online <kellyonline2016@gmail.com>
2019-07-27 15:33:34 +0100

    manipulating selected DOM elements

--- Learnable/LearnDOM/app.js
+++ Learnable/LearnDOM/app.js
@@ -1,3 +1,29 @@
 // document.querySelector('body').style.backgroundColor="blue";
 
-document.body.style.background = "green"
+// document.body.style.background = "green";
+
+document.getElementById('myID')
+
+document.getElementById('myID').innerText;
+
+document.querySelector('ul');
+<ul class=​"second">​<li class=​"first red">​…​</li>​<li class=​"first">​…​</li>​<li>​…​</li>​<li>​…​</li>​<li class=​"first">​…​</li>​<li class=​"first">​…​</li>​<li>​…​</li>​<a href=​"#">​Link Seven​</a>​</li>​<li>​</li>​</ul>​
+
+x = document.createElement('li');
+<li>​</li>​
+
+
+document.querySelector('ul').appendChild(x);
+<li>​</li>​
+
+document.getElementsByTagName('span')
+
+document.querySelector('span:last-child')
+
+document.querySelector('span:first-child')
+
+document.querySelector('span:last-child').style.backgroundColor = 'red';
+
+document.querySelector('span:nth-child(3)').style.backgroundColor = 'red';
+
+
