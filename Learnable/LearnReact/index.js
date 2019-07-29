b0d149529b7d6b7a063d777375154ecd65d967e7
Kelly Online <Kellyonline2016@gmail.com>
2019-07-29 21:23:09 +0100

    add readable comment to code

--- Learnable/LearnReact/index.js
+++ Learnable/LearnReact/index.js
@@ -1,7 +1,11 @@
+// create a variable and store a new react element.
+
 const Header = React.createElement(
   "h2",
   null,
   "Welcome, this is my first react code"
 );
+
+// Using ReactDom and render method, pass the fields
 
 ReactDOM.render(Header, document.getElementById("parameter"));
