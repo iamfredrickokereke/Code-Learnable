dc90e87c7af8f5a999fcaee1c71446a98ad9c956
Kelly Online <Kellyonline2016@gmail.com>
2019-07-29 21:20:09 +0100

    test first react code

--- Learnable/LearnReact/index.js
+++ Learnable/LearnReact/index.js
@@ -1,1 +1,7 @@
-var Header = React.createElement();
+const Header = React.createElement(
+  "h2",
+  null,
+  "Welcome, this is my first react code"
+);
+
+ReactDOM.render(Header, document.getElementById("parameter"));
